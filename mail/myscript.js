606f06ade6f1cb003345a65ad3750a8d7eb67de3
AlessioAbbati <alex_117@live.it>
2023-04-17 16:41:36 +0200

    add const askmail and array with listmail

---
+++ mail/myscript.js
@@ -0,0 +1,13 @@
+// chiedere all'utente la propria email
+// creare un array con le email valide
+
+
+const askMail = prompt("inserisci la tua email");
+
+let listMail = [
+    "uno@live.it",
+    "due@live.it",
+    "tre@live.it",
+    "quattro@gmail.com",
+    "cinque@gmail.com",
+];
