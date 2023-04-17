3a692aa953f87426bff20f72fae1ae663082dba4
AlessioAbbati <alex_117@live.it>
2023-04-17 17:17:57 +0200

    add random number for user and pc

--- dadi/myscript.js
+++ dadi/myscript.js
@@ -1,3 +1,11 @@
 // creo un array con i risultati del dado
 
 let dado = [1,2,3,4,5,6];
+
+// genero casualmente due numeri, per l'utente e per il pc
+
+let numUtente= Math.floor(Math.random(1) * 6) + 1;
+console.log(numUtente);
+
+let numPc= Math.floor(Math.random(1) * 6) + 1;
+console.log(numPc);
